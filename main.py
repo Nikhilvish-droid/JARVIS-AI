bcef416ebdd7a0746d7fa68a34883969e594aabb
nikhilvish847-droid <nikhil.vish847@gmail.com>
2025-11-15 22:56:17 +0530

    giving some insructions and working

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import speech_recognition as sr
 import pyttsx3
-import datetime
+import time
 import pyjokes 
 import win32com.client as w
 import time
 
+# converting users voice into text data.
 def sptext():
     recogniser = sr.Recognizer()
     with sr.Microphone() as source:
@@ -14,10 +15,12 @@ def sptext():
         try:
             print("recognising...")
             data=recogniser.recognize_google(audio)
-            print(data)
+            print(f"YOUR TEXT IS :{data}")
+            return data
         except:
-            print(" not understand")
+            textsp("sorry i cant hear you")
 
+#converting text into computer/jarvis ai voice.
 def textsp(data):
     engine=pyttsx3.init()
     voices=engine.getProperty("voices")
@@ -27,5 +30,23 @@ def textsp(data):
     engine.say(data)
     engine.runAndWait()
 
-sptext()
-textsp(" hello!, nikhil what a shit are you talking man .")
+if __name__=="__main__":
+    ans="y"
+    while ans=="y":
+        data1=sptext()
+        if data1=="hello":
+            textsp("hello hello nikhil what can i help you.")
+
+        elif "time" in str(data1):
+            hour=time.strftime("%H")
+            min=time.strftime("%M")
+            sec=time.strftime("%S")
+            textsp(f"oo,{hour} hour {min}minutes and {sec}seconds")
+
+        elif "name" in str(data1) :
+            textsp(" my my name is JARVIS AI. ")
+            
+        
+        ans=input("enter y/n to activate JARVIS AI :" \
+        "")
+        
